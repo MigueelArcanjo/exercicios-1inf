14e16750bbd851c0a5f6e4021459e14286b3aa06
Miguel Arcanjo <miguel.silva266@aluno.ce.gov.br>
2026-09-13 13:02:15 +0000

    Projeto 2v2 finalizado

--- 3-bimestre/logica-de-programacao/projetos/projeto2v2/MigueelArcanjo/index.js
+++ 3-bimestre/logica-de-programacao/projetos/projeto2v2/MigueelArcanjo/index.js
@@ -1,5 +1,5 @@
 const aluno = "Leandro Costa"
-const plano = "Plano mensal"
+const plano = "Plano Mensal"
 const valorMensalidade = 120
 const taxaMatricula = 30
 const idade = 22
@@ -13,6 +13,7 @@ let descontoStatus
 const valorFinal = valorBase - valorDesconto
 let pagamentoStatus
 const troco = valorPago - valorFinal
+let statusMatricula 
 
 if (idade >= 16) {
     idadeStatus = "Idade permitida"
@@ -21,9 +22,9 @@ if (idade >= 16) {
 }
 
 if (categoriaEstudante == 1) {
-    descontoStatus ="Desconto para estudante aplicado"
+    descontoStatus ="Desconto de estudante aplicado"
 } else {
-    descontoStatus ="Desconto para estudante não aplicado"
+    descontoStatus ="Desconto de estudante não aplicado"
 }
 
 if (valorPago >= valorFinal) {
@@ -32,9 +33,31 @@ if (valorPago >= valorFinal) {
     pagamentoStatus = "Matrícula com saldo pendente"
 }
 
+if (idade >= 16) {
+    if (valorPago >= valorFinal) {
+        statusMatricula = "Matrícula confirmada"
+    } else {
+        statusMatricula = "Matrícula confirmada com saldo pendente"
+} }
+ else {
+     statusMatricula = "Matrícula não pode ser confirmada: idade não permitida"
+    }
+
 const resumo = `
-Aluno = ${aluno}
+Aluno = ${aluno} (${idadeStatus})
+Idade = ${idade}
 Plano de matrícula = ${plano}
+Valor pago = R$ ${valorPago}
+Valor da mensalidade = R$ ${valorMensalidade}
+Taxa de matrícula = R$ ${taxaMatricula}
+Categoria (1 = estudante, 0 = não estudante) = ${categoriaEstudante} (${descontoStatus})
+Se for estudante, valor do desconto: ${valorDesconto}
+Valor total = R$ ${valorBase}
+Valor com desconto estudante = ${valorFinal}
+Troco = ${troco}
+
+Status da matrícula = ${statusMatricula}
+${pagamentoStatus}
 `
 
 console.log(resumo)
